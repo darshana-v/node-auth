f093e176e44974394beab5f6c7637d5fc4928c61
Darshana <vs_darshana@yahoo.com>
2021-09-01 16:42:41 +0530

    user register validation and login done

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+// connect to db
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
+  console.log("Connected to mongodb")
+);
+
+// middleware
+app.use(express.json());
+
+// route middleware
+app.use("/api/user", authRoute);
+
+app.listen(3000, () => console.log("Server running"));
